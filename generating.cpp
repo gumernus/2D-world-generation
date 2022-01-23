60e11189622d9ca300343ea7781f4195d59ef004
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2022-01-23 13:45:05 +0100

    Update generating.cpp

--- generating.cpp
+++ generating.cpp
@@ -3,19 +3,29 @@
 #include <iostream>
 #include "include/raylib.h"
 
-void drawPixel(){
+const float screenWidth = 1920/2;
+const float screenHeight = 1080/2;
 
+const int pixel = 16;
+const int maxPixelsWidth = screenWidth/pixel;
+const int maxPixelsHeight = screenHeight/pixel;
+
+void drawColumn(int x, int y){
+        for (int i = y*pixel; i <= maxPixelsHeight*pixel; i = i + pixel){
+        if (i == y*pixel){
+            DrawRectangle(pixel*x, i, pixel, pixel, PURPLE);    
+        }
+        else if (i == maxPixelsHeight*pixel){
+            DrawRectangle(pixel*x, i, pixel, pixel, GREEN);    
+        }
+        else{
+            DrawRectangle(pixel*x, i, pixel, pixel, BLUE);    
+        }
+    }
 }
 
 int main(void)
 {
-    const float screenWidth = 1920/2;
-    const float screenHeight = 1080/2;
-
-    const int pixel = 16;
-    const int maxPixelsWidth = screenWidth/pixel;
-    const int maxPixelsHeight = screenHeight/pixel;
-
     InitWindow(screenWidth, screenHeight, "Gumernus generating world");
     SetTargetFPS(360);
 
@@ -28,39 +38,33 @@ int main(void)
 
         //----------------
 
-        // // vyrenderovat 0Y maxX pixelů
-        for (int i = 64; i <= maxPixelsWidth*pixel-pixel; i = i + pixel){
-            if (i == 64){
-                DrawRectangle(i, 0, pixel, pixel, PURPLE);        
-            }
-            else if (i == maxPixelsWidth*pixel-pixel){
-                DrawRectangle(i, 0, pixel, pixel, GREEN);      
-            }
-            else{
-                DrawRectangle(i, 0, pixel, pixel, RED);      
-            }
-            
+        // // vyrenderovat řádek pixelů
+        // for (int i = 64; i <= maxPixelsWidth*pixel-pixel; i = i + pixel){
+        //     if (i == 64){
+        //         DrawRectangle(i, 0, pixel, pixel, PURPLE);        
+        //     }
+        //     else if (i == maxPixelsWidth*pixel-pixel){
+        //         DrawRectangle(i, 0, pixel, pixel, GREEN);      
+        //     }
+        //     else{
+        //         DrawRectangle(i, 0, pixel, pixel, RED);      
+        //     }
+        // }
+
+        // vyrenderovat sloupec pixelů
+        for (int i = 0; i < maxPixelsWidth; i++){
+            int randomNumber = 0 + ( std::rand() % ( maxPixelsHeight - 0 + 1 ) );
+            drawColumn(i,randomNumber);
+            // drawColumn(i, i);
         }
 
-        // vyrenderovat maxY 0X pixelů
-        for (int i = 64; i <= maxPixelsHeight*pixel; i = i + pixel){
-            if (i == 64){
-                DrawRectangle(0, i, pixel, pixel, PURPLE);    
-            }
-            else if (i == maxPixelsHeight*pixel){
-                DrawRectangle(0, i, pixel, pixel, GREEN);    
-            }
-            else{
-                DrawRectangle(0, i, pixel, pixel, BLUE);    
-            }
-        }
 
         //----------------
 
         DrawText(TextFormat("screenWidth: %.0f", screenWidth), 20, screenHeight-30, 20, LIGHTGRAY);
         DrawText(TextFormat("screenHeight: %.0f", screenHeight), 20, screenHeight-50, 20, LIGHTGRAY);
-        DrawText(TextFormat("maxPixelsWidth : %.0i", maxPixelsWidth), 20, screenHeight-70, 20, LIGHTGRAY);
-        DrawText(TextFormat("maxPixelsHeight : %.0i", maxPixelsHeight), 20, screenHeight-90, 20, LIGHTGRAY);
+        DrawText(TextFormat("maxPixelsWidth: %.0i", maxPixelsWidth), 20, screenHeight-70, 20, LIGHTGRAY);
+        DrawText(TextFormat("maxPixelsHeight: %.0i", maxPixelsHeight), 20, screenHeight-90, 20, LIGHTGRAY);
 
         //----------------
 
