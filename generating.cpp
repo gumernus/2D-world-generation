d50370e86041c1e88a434ece94d133731cda96e2
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2022-01-23 12:57:57 +0100

    Update generating.cpp

--- generating.cpp
+++ generating.cpp
@@ -13,8 +13,8 @@ int main(void)
     const float screenHeight = 1080/2;
 
     const int pixel = 16;
-    const float maxPixelsWidth = screenWidth/pixel;
-    const float maxPixelsHeight = screenHeight/pixel;
+    const int maxPixelsWidth = screenWidth/pixel;
+    const int maxPixelsHeight = screenHeight/pixel;
 
     InitWindow(screenWidth, screenHeight, "Gumernus generating world");
     SetTargetFPS(360);
@@ -28,14 +28,31 @@ int main(void)
 
         //----------------
 
-        // vyrenderovat 0Y maxX pixelů
-        for (int i = 0; i < maxPixelsWidth*pixel; i = i + pixel){
-            DrawRectangle(i, 0, pixel, pixel, RED);    
+        // // vyrenderovat 0Y maxX pixelů
+        for (int i = 0; i <= maxPixelsWidth*pixel-pixel; i = i + pixel){
+            if (i == 0){
+                DrawRectangle(i, 0, pixel, pixel, PURPLE);        
+            }
+            else if (i == maxPixelsWidth*pixel-pixel){
+                DrawRectangle(i, 0, pixel, pixel, GREEN);      
+            }
+            else{
+                DrawRectangle(i, 0, pixel, pixel, RED);      
+            }
+            
         }
 
         // vyrenderovat maxY 0X pixelů
-        for (int i = 0; i < maxPixelsHeight*pixel; i = i + pixel){
-            DrawRectangle(0, i, pixel, pixel, BLUE);    
+        for (int i = 0; i <= maxPixelsHeight*pixel; i = i + pixel){
+            if (i == 0){
+                DrawRectangle(0, i, pixel, pixel, PURPLE);    
+            }
+            else if (i == maxPixelsHeight*pixel){
+                DrawRectangle(0, i, pixel, pixel, GREEN);    
+            }
+            else{
+                DrawRectangle(0, i, pixel, pixel, BLUE);    
+            }
         }
 
         //----------------
@@ -44,6 +61,7 @@ int main(void)
         DrawText(TextFormat("screenHeight: %.0f", screenHeight), 10, screenHeight-50, 20, LIGHTGRAY);
         DrawText(TextFormat("maxPixelsWidth : %.0f", maxPixelsWidth), 10, screenHeight-70, 20, LIGHTGRAY);
         DrawText(TextFormat("maxPixelsHeight : %.0f", maxPixelsHeight), 10, screenHeight-90, 20, LIGHTGRAY);
+        DrawText(TextFormat("maxPixelsHeight*pixel : %.0f", maxPixelsHeight*pixel), 10, screenHeight-110, 20, LIGHTGRAY);
 
         //----------------
 
